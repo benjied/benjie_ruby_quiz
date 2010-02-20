bc103b63710443dcd5378cf911c25949767c8324
benjied <lifeisgood@gmail.com>
2010-02-20 12:11:01 -0600

    Did a bunch of work, but broken right now

--- solitaire_cipher/solitaire_cipher.rb
+++ solitaire_cipher/solitaire_cipher.rb
@@ -7,16 +7,59 @@
 # git remote add origin git@github.com:benjied/ruby_quiz.git
 # git push origin master
 class SolitaireCipher
+  input_message = ARGV[0]
 
-  def self.encrypt(message)
+  if input_message =~ %{^[A-Z][A-Z][A-Z][A-Z][A-Z]( [A-Z][A-Z][A-Z][A-Z][A-Z])*}
+#    input_message.decrypt
+  else
+#    encrypt(input_message)    
+  end
+
+  def self.package(message)
     stripped_message = message.gsub(/\W/, '').upcase
-    length = stripped_message.length
-    number_of_groups = length/5
-    array_of_strings = []
-    number_of_groups.times do |offset|
-      array_of_strings << stripped_message[5*offset..5*offset+4]
-    end
-    array_of_strings.join(' ')
   end
   
+  def self.convert_to_numbers(message)
+    message_in_array = message.split(//)
+    
+    message_in_ascii = []
+    message_in_array.each { |character| message_in_ascii <<  character[0]  }
+    
+    message_in_numbers = []
+    message_in_ascii.each { |number| message_in_numbers <<  number-64  }
+    message_in_numbers
+  end
+  
+  def self.convert_to_letters(numbered_code)
+    message_in_letters = []
+    numbered_code.each { |number| message_in_letters << (number + 64).chr }
+    message_in_letters
+  end
+  
+  def self.encrypt(message)
+    packaged_message = package(message)
+    numbered_message = convert_to_numbers(packaged_message)
+    puts numbered_message.length
+    numbered_keystream = convert_to_numbers(Deck.generate_keystream(numbered_message.length))
+    numbered_code = []
+    numbered_message.each_index do |index|
+      number = numbered_message[index] + numbered_keystream[index]
+      number = number - 26 if numberr > 26
+      numbered_code << number
+    end
+    coded_message = convert_to_letters(numbered_code)
+    
+    number_of_groups = packaged_message.length/5
+    array_of_strings = []
+    number_of_groups.times do |offset|
+      array_of_strings << coded_message[5*offset..5*offset+4]
+    end
+    coded_message = array_of_strings.join(' ')
+    
+    puts coded_message
+    
+  end
+
+  
 end
+
--- solitaire_cipher/solitaire_cipher_test.rb
+++ solitaire_cipher/solitaire_cipher_test.rb
@@ -1,13 +1,107 @@
 require 'test/unit'
 require 'solitaire_cipher'
+require 'deck'
 
 class SolitaireCipherTest < Test::Unit::TestCase
 
-  def test_step_one_of_encryption
-    assert_equal 'CODEI NRUBY LIVEL ONGER', SolitaireCipher.encrypt("Code in Ruby, live longer!")
+  def test_step_one_of_encryption_package
+    assert_equal 'CODEINRUBYLIVELONGER', SolitaireCipher.package("Code in Ruby, live longer!")
   end
   
-  # def test_solitaire_cipher_encryption
-  #   assert_equal 'GLNCQ MJAFF FVOMB JIYCB', SolitaireCipher.encrypt("Code in Ruby, live longer!")
-  # end
+  def test_step_three_of_encryption_make_numbers
+    assert_equal [3,15,4,5,9,14,18,21,2,25,12,9,22,5,12,15,14,7,5,18], SolitaireCipher.convert_to_numbers('CODEINRUBYLIVELONGER')
+  end
+  
+  def test_convert_to_letters
+    assert_equal ['C','O','D','E','I','N','R','U','B','Y','L','I','V','E','L','O','N','G','E','R'], SolitaireCipher.convert_to_letters([3,15,4,5,9,14,18,21,2,25,12,9,22,5,12,15,14,7,5,18])
+  end
+  
+  def test_a_new_clean_deck
+    assert_equal [1,2,3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,
+                        20,21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,
+                        40,41,42,43,44,45,46,47,48,49,
+                        50,51,52,'A','B'], 
+                        Deck.make_deck
+  end
+                      
+  def test_move_joker_A_down_one_card
+    @deck = Deck.make_deck
+    assert_equal [1,2,3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,
+                        20,21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,
+                        40,41,42,43,44,45,46,47,48,49,
+                        50,51,52,'B','A'],
+                        Deck.move_joker_A_down_one
+  end
+  
+  def test_move_joker_B_down_two_cards
+    @deck = Deck.make_deck
+    @deck = Deck.move_joker_A_down_one
+
+    assert_equal [1,'B',2,3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,
+                        20,21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,
+                        40,41,42,43,44,45,46,47,48,49,
+                        50,51,52,'A'],
+                        Deck.move_joker_B_down_two
+  end
+
+
+  def test_triple_cut
+    @deck = Deck.make_deck
+    @deck = Deck.move_joker_A_down_one
+    @deck = Deck.move_joker_B_down_two
+    assert_equal ['B',2,3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,
+                        20,21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,
+                        40,41,42,43,44,45,46,47,48,49,
+                        50,51,52,'A',1],
+                        Deck.triple_cut
+  end
+  
+  def test_count_cut
+    @deck = Deck.make_deck
+    @deck = Deck.move_joker_A_down_one
+    @deck = Deck.move_joker_B_down_two
+    @deck = Deck.triple_cut
+    
+    assert_equal [2,3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,
+                        20,21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,
+                        40,41,42,43,44,45,46,47,48,49,
+                        50,51,52,'A','B',1],
+                        Deck.count_cut
+  end                        
+
+  def test_output_letter
+    @deck = Deck.make_deck
+    @deck = Deck.move_joker_A_down_one
+    @deck = Deck.move_joker_B_down_two
+    @deck = Deck.triple_cut
+    @deck = Deck.count_cut
+    assert_equal 'D', Deck.output_letter
+  end
+  
+  def test_generate_keystream(length)
+    @deck = Deck.make_deck
+    @deck = Deck.move_joker_A_down_one
+    @deck = Deck.move_joker_B_down_two
+    @deck = Deck.triple_cut
+    @deck = Deck.count_cut
+    assert_equal 'DWJXHYRFDGTMSHPUURXJ', Deck.generate_keystream(25)
+  end
+    
+
+
+  def test_solitaire_cipher_encryption
+    assert_equal 'GLNCQ MJAFF FVOMB JIYCB', SolitaireCipher.encrypt("Code in Ruby, live longer!")
+  end
+  
 end
+
